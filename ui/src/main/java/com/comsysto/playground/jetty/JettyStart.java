8fbdd1b2c631b9d872c90ba8ee9ad6df8dd8827c
Elisabeth Engel <Elisabeth.Engel@comsysto.com>
2013-06-19 13:41:34 +0200

    Changed files for Gradle Application use

--- ui/src/main/java/com/comsysto/playground/jetty/JettyStart.java
+++ ui/src/main/java/com/comsysto/playground/jetty/JettyStart.java
@@ -1,15 +1,22 @@
-package com.comsysto.playground.ui.runner;
+package com.comsysto.playground.jetty;
 
 public class JettyStart {
 
     public static void main(final String[] args) {
+
+        int port = 8080;
+
         if (args.length < 1) {
             System.out.println("JettyStart <httpport>");
-            return;
+        }
+        else {
+            port = Integer.valueOf(args[0]);
         }
 
+
         System.setProperty("spring.profiles.active", "default");
-        JettyStarterApplication jettyStarter = new JettyStarterApplication(Integer.valueOf(args[0]));
+
+        JettyStarterApplication jettyStarter = new JettyStarterApplication(port);
         jettyStarter.startServer();
     }
 }
